f7d354720bcc80b5727e7cc01e246fb15037b3da
igor <igor.bubenko@gmail.com>
2020-11-28 03:10:09 +0100

    userId

--- index.js
+++ index.js
@@ -3,74 +3,73 @@ const AWS = require('aws-sdk');
 const { assert } = require('chai');
 
 class DynamoDB {
-  constructor ({ table }) {
-    this.table = table;
-    this.ddb = new AWS.DynamoDB.DocumentClient();
+  constructor ({ table, env = process.env.NODE_ENV, region = process.env.REGION }) {
+    this.table = `lo-${table}-${env}`;
+    this.ddb = new AWS.DynamoDB.DocumentClient({ region });
   }
 
   async get (key) {
-    try {
-      const params = {
-        TableName: this.table,
-        Key: key
-      };
-      const response = await this.ddb.get(params).promise();
-      return response.Item;
-    } catch (e) {
-      // if (e.code === 'ResourceNotFoundException') return null;
-      console.error(e);
-      throw e;
-    }
-  }
-
-  async put (data) {
     const params = {
       TableName: this.table,
-      Item: data
+      Key: key
     };
-    await this.ddb.put(params).promise();
-    return data;
+    const response = await this.ddb.get(params).promise();
+    return response.Item;
   }
 
   async create ({ data, userId }) {
     assert.isDefined(data.id);
+    data = {
+      ...data,
+      meta: {
+        version: 1,
+        createdAt: new Date().toISOString(),
+        createdBy: userId
+      }
+    };
     const params = {
       TableName: this.table,
-      Item: {
-        ...data,
-        meta: {
-          version: 1,
-          createdAt: new Date().toISOString(),
-          createdBy: userId
-        }
-      },
+      Item: data,
       ConditionExpression: 'attribute_not_exists(id)'
     };
     await this.ddb.put(params).promise();
     return data;
   }
 
-  async save ({ data, userId }) {
-    assert.isDefined(data.id);
-    assert.isDefined(data.meta);
-    const orig = await this.get({ id: data.id });
-    assert.equal(orig.meta.version, data.meta.version, `expected version ${orig.meta.version}, got ${data.meta.version}`);
-    data = {
-      ...data,
-      meta: {
-        ...orig.meta,
-        version: orig.meta.version + 1,
-        modifiedBy: userId,
-        modifiedAt: new Date().toISOString()
+  async update ({ key, data, userId }) {
+    assert.isObject(key);
+    assert.isObject(data);
+    assert.isDefined(userId);
+    let params;
+    try {
+      const ean = { '#meta': 'meta', '#version': 'version', '#modifiedAt': 'modifiedAt', '#modifiedBy': 'modifiedBy' };
+      const set = ['#meta.#modifiedBy = :userId', '#meta.#modifiedAt = :now'];
+      const attrs = { ':userId': userId, ':now': new Date().toISOString(), ':increment': 1 };
+      let varindex = 1;
+      for (const name in data) {
+        const value = data[name];
+        if (value === undefined) continue;
+        attrs[`:var_${varindex}`] = value;
+        name.split('.').forEach(n => { ean[`#${n}`] = n; });
+        set.push(`${name.split('.').map(n => `#${n}`).join('.')} = :var_${varindex}`);
+        varindex += 1;
       }
-    };
-    const params = {
-      TableName: this.table,
-      Item: data,
-      ConditionExpression: 'attribute_exists(id)'
-    };
-    const result = await this.ddb.put(params).promise();
-    return result;
+      params = {
+        TableName: this.table,
+        Key: key,
+        UpdateExpression: `SET ${set.join(', ')} ADD #meta.#version :increment`,
+        ExpressionAttributeValues: attrs,
+        ExpressionAttributeNames: ean,
+        ReturnValues: 'ALL_NEW',
+        ConditionExpression: 'attribute_exists(id)'
+      };
+      const result = await this.ddb.update(params).promise();
+      return result.Attributes;
+    } catch (e) {
+      console.log('params: ', params);
+      console.error(e);
+      throw e;
+    }
   }
 
   async delete (key) {
@@ -82,6 +81,7 @@ class DynamoDB {
   }
 
   async query ({ index, expression, filter, values, attrs }) {
+    // TODO iterate via all items
     const params = {
       TableName: this.table,
       KeyConditionExpression: expression,
@@ -94,12 +94,17 @@ class DynamoDB {
     return response.Items;
   }
 
-  async scan ({ projection } = {}) {
+  async scan ({ projection, expression, values } = {}) {
     let list = [];
     const params = {
-      TableName: this.table
+      TableName: this.table,
+      ProjectionExpression: projection
     };
-    if (projection) params.ProjectionExpression = projection;
+    if (expression) params.FilterExpression = expression;
+    if (values) {
+      params.ExpressionAttributeValues = {};
+      for (const k in values) params.ExpressionAttributeValues[`:${k}`] = values[k];
+    }
     do {
       const response = await this.ddb.scan(params).promise();
       params.ExclusiveStartKey = response.LastEvaluatedKey;
@@ -108,57 +113,6 @@ class DynamoDB {
     return list;
   }
 
-  async update (key, data) {
-    const set = [];
-    const attrs = {};
-    const ean = {};
-    for (const name in data) {
-      const value = data[name];
-      if (value === undefined) continue;
-      attrs[`:${name}`] = value;
-      ean[`#${name}`] = name;
-      if (_.isArray(value)) {
-        attrs[':empty_list'] = [];
-        set.push(`#${name} = list_append(if_not_exists(#${name}, :empty_list), :${name})`);
-      } else {
-        set.push(`#${name} = :${name}`);
-      }
-    }
-    const params = {
-      TableName: this.table,
-      Key: key,
-      UpdateExpression: `SET ${set.join(', ')}`,
-      ExpressionAttributeValues: attrs,
-      ExpressionAttributeNames: ean,
-      ReturnValues: 'ALL_NEW'
-    };
-
-    const result = await this.ddb.update(params).promise();
-    return result.Attributes;
-  }
-
-  async patch (key, data) {
-    const set = [];
-    const attrs = {};
-    const names = {};
-    for (const name in data) {
-      const v = name.split('.');
-      attrs[`:variable_${set.length}`] = data[name];
-      set.push(`${v.map(i => `#${i}`).join('.')} = :variable_${set.length}`);
-      v.forEach(n => { names[`#${n}`] = n; });
-    }
-    const params = {
-      TableName: this.table,
-      Key: key,
-      UpdateExpression: `SET ${set.join(', ')}`,
-      ExpressionAttributeValues: attrs,
-      ExpressionAttributeNames: names,
-      ReturnValues: 'ALL_NEW'
-    };
-    console.log(params);
-    const result = await this.ddb.update(params).promise();
-    return result.Attributes;
-  }
 }
 
 module.exports = DynamoDB;
